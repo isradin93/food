c28ec1fc7fbeefeb609ee73d04107e456cc6b39b
Isradin <isradin.nurbek@gmail.com>
2021-03-08 20:04:15 +0300

    Feat: Add food project

---
+++ js/script.js
@@ -0,0 +1,236 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    // Tabs
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items'),
+        tabs = tabsParent.querySelectorAll('.tabheader__item');
+
+    const hideTabsContent = () => {
+        tabsContent.forEach(tabContent => {
+            tabContent.classList.add('hide');
+            tabContent.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        });
+    };
+
+    const showTabsContent = (i = 0) => {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
+        tabs[i].classList.add('tabheader__item_active');
+    };
+
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const eventTarget = event.target;
+
+        if (eventTarget && eventTarget.classList.contains('tabheader__item')) {
+            tabs.forEach((tab, i) => {
+                if (eventTarget === tab) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                }
+            });
+        }
+    });
+
+    // Timer
+
+    const deadline = '2021-04-10'; // Deadline of promotions
+
+    // Function calculates(gets) difference between deadline and current time
+    const getTimeRemaining = (endTime) => {
+        // var t gets difference between these two dates in milliseconds
+        const timeDifference = Date.parse(endTime) - new Date(),
+
+            /* var days - gets quantity milliseconds divide quantity milliseconds of 
+            one day with Math.floor(округление вниз). Math.floor( (1000 * 60) - quantity milliseconds in a minute).
+            Math.floor( (1000 * 60 * 60) - quantity milliseconds in a hour).
+            Math.floor( (1000 * 60 * 60 * 24) - quantity milliseconds in 24 hours) */
+            days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
+
+            /* var hours - gets quantity milliseconds divide quantity milliseconds of 
+            one hour with Math.floor(округление вниз).
+            Math.floor( (1000 * 60 * 60) - quantity milliseconds in a hour).
+            Math.floor( (1000 * 60 * 60) % 24) - оператор остатка (%) делить на 24 часа, получим хвостик
+            которого не хватает до полных суток чтобы хвостик был не больше 24 часа) */
+            hours = Math.floor((timeDifference / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((timeDifference / 1000 / 60) % 60),
+            seconds = Math.floor((timeDifference / 1000) % 60);
+
+        return {
+            'total': timeDifference, // if timer is finished
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    };
+
+    // Function puts zero before hours
+    const addZero = (num) => {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    };
+
+    // Function sets clock on the page
+    const setClock = (selectorTimer, endTime) => {
+        const timer = document.querySelector(selectorTimer), // parameter selector for more timers in the page
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); // Stops blinking layout (Мигание верстки) when we update the page
+
+        function updateClock() {
+            const timeRemainingObj = getTimeRemaining(endTime); // Inside object of function getTimeRemaining
+
+            days.textContent = addZero(timeRemainingObj.days);
+            hours.textContent = addZero(timeRemainingObj.hours);
+            minutes.textContent = addZero(timeRemainingObj.minutes);
+            seconds.textContent = addZero(timeRemainingObj.seconds);
+
+            if (timeRemainingObj.total <= 0) { // Stops timer when deadline time is done
+                clearInterval(timeInterval);
+            }
+        }
+    };
+
+    setClock('.timer', deadline);
+
+    // Modal
+    const modalTriggerBtn = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close-modal]');
+
+    // Open modal dialog function
+    const openModal = () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        //modal.classList.toggle('show');  // Toggle version
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId); // To not open modal after open by user
+    };
+
+    modalTriggerBtn.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    // Close modal dialog function
+    const closeModal = () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.classList.toggle('show');  // Toggle version
+        document.body.style.overflow = '';
+    };
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    // Close modal by clicking the modal field
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    // Close modal by clicking Esc keyword
+    window.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape' && modal.classList.contains('show')) {
+            // Close modal by Esc only  when modal dialog is open
+            closeModal();
+        }
+    });
+
+    // Open modal in 5 minutes
+    //const modalTimerId = setTimeout(openModal, 5000);
+
+    // Open modal when user scrolled page till the end
+    const openModalByScroll = () => {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', openModalByScroll);
+        }
+    };
+
+    window.addEventListener('scroll', openModalByScroll);
+
+    // Using class for cards
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.parent = document.querySelector(parentSelector);
+                this.classes = classes; // Array
+                this.transfer = 27;
+                this.changeToUAH();
+            }
+            // Convert money FROM US$ to UAH
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        // Create layout
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9, // US$
+        ".menu .container"
+    ).render(); // When we need to use a single time
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        'Меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14, // US$
+        ".menu .container"
+    ).render(); // When we need to use a single time
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21, // US$
+        ".menu .container"
+    ).render(); // When we need to use a single time
+});
